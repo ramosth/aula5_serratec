36b7c34478883f4df670c8af7b851dceaa382ae4
Thamires Ramos <thamiresramos014@gmail.com>
2021-04-06 16:29:53 -0300

    versao final

--- js/teste.js
+++ js/teste.js
@@ -1,7 +1,14 @@
 var a = 10, b = 11, c = 12;
 
-function soma() {
-  return a+b+c;
+function funcoesBase() {
+  let soma = a*b*c;
+  let multiplica = a+b+c;
+  let divisao = a/b/c;
+  let subtracao = a-b-c;
+  document.write(`A soma será ${soma} <br>`);
+  document.write(`A subtracao será ${multiplica} <br>`);
+  document.write(`A divisão será ${divisao} <br>`);
+  document.write(`A subtração será ${subtracao} <br>`);
 }
 
-soma();
+funcoesBase();
